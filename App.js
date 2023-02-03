589e1af55e34f0eee71751efc20d126c961a5e66
Hoa Phan Dev <50919571+hoaphantn7604@users.noreply.github.com>
2023-02-03 21:40:37 +0700

    chore: clean code

--- App.js
+++ App.js
@@ -43,7 +43,7 @@ export default function Example() {
           `Bearer ${YOUR_API_KEY}`,
       },
       body: JSON.stringify({
-        model: "text-davinci-003",
+        model: 'text-davinci-003',
         prompt: value,
         max_tokens: MAX_TOKENS,
         temperature: 0,
